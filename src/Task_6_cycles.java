3fd61575806f7d12ceadfe525ccf4eaae81ce756
Olga Kemezh <lolatal92@gmail.com>
2021-04-14 13:16:19 +0300

    changed variable

--- src/Task_6_cycles.java
+++ src/Task_6_cycles.java
@@ -1,6 +1,6 @@
 public class Task_6_cycles {
     public static void main(String[] args) {
-        int square=1;
+        int square;
         for(int i=10; i<=20; i++){
             square=i*i;
             System.out.println("Квадрат числа  "+ i + " = " + square);
